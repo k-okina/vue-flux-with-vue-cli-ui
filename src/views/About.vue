655b7517a03d3107750e91db87c8723dfc82c343
k-okina <okinakahiro@gmail.com>
2018-07-13 08:38:19 +0900

    [add] tsでVueちょっと書いてみた

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>{{ counter }}</div>
+    <button @click="inc">Add Count</button>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    inc(): void {
+      this.counter++;
+    },
+  },
+});
+</script>
